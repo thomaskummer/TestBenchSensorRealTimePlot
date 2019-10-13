a5ef4d34df5de77287ddb298fff336edd34bc37a
ejoos <ejoos@ethz.ch>
2019-10-13 15:00:30 +0200

    add getmmhg2

--- Client/client.py
+++ Client/client.py
@@ -8,13 +8,15 @@ import time
 class Client(object):
     """This class describes the client running on the raspberry pi """
 
-    def __init__(self, address_mp, address_sensor, nb_sensors, host, port, i2c_sw):
+    def __init__(self, address_mp, address_sensor, nb_sensors, host, port, i2c_sw,i2c_switch2,i2c_switch3):
         self.address_mp = address_mp
         self.address_sensor = address_sensor
         self.nb_sensors = nb_sensors
         self.host = host
         self.port = port
         self.i2c_sw = i2c_sw
+        self.i2c_sw2 = i2c_switch2
+        self.i2c_sw3 = i2c_switch3
 
     def client(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -23,27 +25,49 @@ class Client(object):
             while True:
                 data = []
                 t = time.time() - start
-                data.append(t)
-                if t >= 20.0:
+                if t >= 10000:
                     start = time.time()
                     t = time.time() - start
                 data.append(t)
 
-                for i in range(self.nb_sensors):
+                for i in range(0,8):
                     self.i2c_sw.chn(i)
                     sensor = self.i2c_sw.get_mmhg(self.i2c_sw.get_data())
                     data.append(sensor)
-                    time.sleep(0.001)
-                    s.sendall(pickle.dumps(data))
+                   # time.sleep(0.005)
+                self.i2c_sw._rst()
+                for i in range(0,2):#4
+                    self.i2c_sw2.chn(i)
+                    sensor2 = self.i2c_sw2.get_mmhg2(self.i2c_sw2.get_data())
+                    data.append(sensor2)
+                   # time.sleep(0.005)
+                #self.i2c_sw2._rst()
+                for i in range(2,6):
+                    self.i2c_sw2.chn(i)
+                    sensor2 = self.i2c_sw2.get_mmhg(self.i2c_sw2.get_data())
+                    data.append(sensor2)
+                self.i2c_sw2._rst()
+                for i in range(6,8):#6,8
+                    self.i2c_sw3.chn(i)
+                    sensor3 = self.i2c_sw3.get_mmhg_underpressure(self.i2c_sw3.get_data())
+                    data.append(sensor3)
+                time.sleep(0.05)
+                self.i2c_sw3._rst()
+                s.sendall(pickle.dumps(data))
 
 
 if __name__ == '__main__':
-
-    address_muliplexer = 0x70
+    time.sleep(5)
+    address_multiplexer = 0x70
+    address_multiplexer2 = 0x71
+    address_multiplexer3 = 0x72
     address_sensor = 0x28
-    i2c_switch = I2C_SW("I2C switch 0", address_muliplexer, 1, address_sensor)
+    address_sensor2 = 0x68
+    i2c_switch = I2C_SW("I2C switch 0", address_multiplexer, 1, address_sensor)
+    i2c_switch2 = I2C_SW("I2C switch 1", address_multiplexer2,1,address_sensor)
+    i2c_switch3 = I2C_SW("I2C switch 2",address_multiplexer3,1,address_sensor2)
     HOST = '169.254.115.231'  # The server's hostname or IP address
-    PORT = 6677
+    PORT = 6676
 
-    C = Client(address_muliplexer,address_sensor,6,HOST,PORT,i2c_switch)
+    C = Client(address_multiplexer,address_sensor,6,HOST,PORT,i2c_switch,i2c_switch2,i2c_switch3)
     C.client()
