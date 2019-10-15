4be30756e18a636c6e2f6a701189663af1b7944b
Thomas Kummer <ejoos@ethz.ch>
2019-10-15 10:10:52 +0200

    change pi

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-ssh pi@169.254.115.230 python3 /home/pi/Desktop/digital_sensor_read_out/Client/client.py &
+ssh pi@169.254.230.24 python3 /home/pi/Desktop/digital_sensor_read_out/Client/client.py &
 
 python3 Server/server.py
 
